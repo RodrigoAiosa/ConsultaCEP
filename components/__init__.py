ff9ebfa038088836eeafac24b03f857c022e52ff
Rodrigo Aiosa <32776696+RodrigoAiosa@users.noreply.github.com>
2026-07-06 09:34:13 -0300

    Update __init__.py

--- components/__init__.py
+++ components/__init__.py
@@ -1,9 +1,6 @@
 # components/__init__.py
-"""
-Componentes de UI para cada aba do aplicativo.
-"""
 from . import individual_search
 from . import batch_search
 from . import cep_range
 
-__all__ = ["individual_search", "batch_search", "cep_range"]
+__all__ = ['individual_search', 'batch_search', 'cep_range']
