006aadfb393807cffa3fce5b4aa48b70043fdc09
Rodrigo Aiosa <32776696+RodrigoAiosa@users.noreply.github.com>
2026-07-06 09:34:29 -0300

    Update __init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -1,8 +1,5 @@
 # services/__init__.py
-"""
-Serviços de acesso a APIs externas.
-"""
 from . import viacep
 from . import geocoding
 
-__all__ = ["viacep", "geocoding"]
+__all__ = ['viacep', 'geocoding']
